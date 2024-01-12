88aec6f89a0964da68bbcd9997745f9bee35fd2f
cjpalmer330 <cjpalmer330@gmail.com>
2024-01-12 09:48:14 -0600

    sliders,  randomized velocities
    added gravity strength, simulation speed, and trail length sliders, as well as randomizing initialized velocity vectors of the planets. working on star background

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
+import os.path
 import pygame
 import math
+import random
 
 ## initialize pygame and create window
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("<Your game>")
 clock = pygame.time.Clock()  ## For syncing the FPS
-
+pauseBut = pygame.transform.scale_by(pygame.image.load('pause-button-svgrepo-com.svg'), 0.1)
+playBut = pygame.transform.scale_by(pygame.image.load('play-button-svgrepo-com.svg'),0.1)
+pauseState = False
 running = True
 FPS = 240
+mousePos = pygame.mouse.get_pos()
 
+class Slider:
+    def __init__(self, pos: tuple, size: tuple, initialPercent: float, minVal: float, maxVal: float):
+        self.pos = pos
+        self.size = size
+        self.min = minVal
+        self.max = maxVal
+        self.sliderX = self.pos[0]
+
+        # x position minus or plus half of the width size as to make the position the center point
+        self.sliderLeft = self.pos[0] - (size[0] // 2)
+        self.sliderRight = self.pos[0] + (size[0] // 2)
+        self.sliderTop = self.pos[1] - (size[1] // 2)
+        self.initialValue = (self.sliderRight - self.sliderLeft) * initialPercent
+
+        # rects for display
+        self.containerRect = pygame.Rect(self.pos[0] - (size[0] // 2), self.sliderTop, self.size[0], self.size[1])
+        self.buttonRect = pygame.Rect(self.sliderX, self.sliderTop, 10, self.size[1])
+
+    def moveSlider(self, currentMouse):
+        self.buttonRect.centerx = currentMouse[0]
+
+    def renderSlider(self):
+        pygame.draw.rect(screen, '#8a8a8a', self.containerRect)
+        pygame.draw.rect(screen, '#525252', self.buttonRect)
+
+    def getValue(self):
+        valRange = self.sliderRight - self.sliderLeft - 1
+        buttonVal = self.buttonRect.centerx - self.sliderLeft
+
+        return(buttonVal/valRange)*(self.max - self.min) + self.min
 
 class planet:
     def __init__(self, mass, color, posX, posY, vel, density, lock):
@@ -34,12 +69,45 @@ class planet:
         for trailDot in self.trail:
             pygame.draw.circle(screen, self.color, trailDot, 2)
 
+# randomized velocities for the two planet system
+randBlueX = round(random.uniform(-2.3, 2.3), 4)
+randBlueY = round(random.uniform(-2.3, 2.3), 4)
+greenXLowerBound, greenYLowerBound = -2.5, -2.5
+if randBlueX < 0:
+    greenXLowerBound = 0
+if randBlueY < 0:
+    greenYLowerBound = 0
+randGreenX = round(random.uniform(greenXLowerBound, 2.3), 4)
+randGreenY = round(random.uniform(greenYLowerBound, 2.3), 4)
 
 # declare planets
-gravitationalConstant = 200
-AllPlanets = [planet(300, '#5555FF', 700, 500, [0, 0], 3, True),
-              planet(30, '#55FF55', 900, 500, [0, 20], 1, False),
-              planet(30, '#FF5577', 300, 500, [0, -17], 1, False)]
+AllPlanets = [
+    # planet and moons
+    # planet(300, '#5555FF', 700, 500, [0, 0], 5, True),
+    # planet(30, '#55FF55', 900, 500, [0, 10.7], 1, False),
+    # planet(30, '#FF5577', 400, 500, [0, -9], 1, False)
+
+    # two body system
+    planet(50, "#5555FF", 500, 500, [randBlueX, randBlueY], 1, False),
+    planet(50, "#55FF55", 600, 600, [randGreenX, randGreenY], 1, False)
+              ]
+sliderList = [
+    # gravitational Constant
+    Slider((1800, 100), (150, 20), 0.5, 50, 250),
+    # simulation speed
+    Slider((1800, 150), (150, 20), 0.0, 32, 1024),
+    # trail length
+    Slider((1800, 200), (150, 20), 0.5, 15, 500)
+]
+sliderText = [
+    "Gravitational Constant",
+    "Simulation Speed",
+    "Orbital Trail Length",
+    "Blue X Init Vel: " + str(randBlueX),
+    "Blue Y Init Vel: " + str(randBlueY),
+    "Green X Init Vel: " + str(randGreenX),
+    "Green Y Init Vel: " + str(randGreenY)
+]
 while running:
     clock.tick(FPS)
 
@@ -49,6 +117,55 @@ while running:
             running = False
 
     screen.fill('#000000')
+
+    # drawing background
+    screen.fill('#000000')
+    randX = random.uniform(0, 1) * 1920
+    randY = random.uniform(0, 1) * 1080
+    # print(randX, randY)
+    # drawing randomized stars
+    # if j < 100:
+    #    pygame.draw.polygon(screen, 'white', [(randX, randY),
+    #                                         (randX + 10, randY + 2.25),
+    #                                        (randX + 10 + 2.25, randY + 2.25 + 10),
+    #                                       (randX + 10 + 4.5, randY + 2.25),
+    #                                      (randX + 20 + 4.5, randY),
+    #                                     (randX + 10 + 4.5, randY - 2.25),
+    #                                    (randX + 10 + 2.25, randY - 2.25 - 10),
+    #                                   (randX + 10, randY - 2.25),
+    #                                  (randX, randY)
+    #                                 ], 0)
+
+
+
+    #drawing play/pause button
+    if pauseState:
+        pausePlay = pauseBut
+    else:
+        pausePlay = playBut
+    screen.blit(pausePlay, (1800, 400))
+
+    # Displaying all sliders
+    for slider in sliderList:
+        slider.renderSlider()
+        # calculating mouse interaction
+        if slider.containerRect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
+            slider.moveSlider(pygame.mouse.get_pos())
+
+    # getting slider values
+    gravitationalConstant = sliderList[0].getValue()
+    FPS = sliderList[1].getValue()
+    trailLength = sliderList[2].getValue()
+
+    # slider labels
+    i = 0
+    for sliderLabel in sliderText:
+        font = pygame.font.Font(pygame.font.get_default_font(), 16)
+        sliderTextSurface = font.render(sliderLabel, True, 'white', 'black')
+        screen.blit(sliderTextSurface, (1720, (i * 50) + 108 - 36))
+
+        i += 1
+
 
     # calculating gravity between planets
     for body in AllPlanets:
